c92142268325b8a8cceb7080138979df674e04ca
David Ukelere <david.ukelere291@gmail.com>
2022-12-14 12:33:46 +0100

    Added 1main

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - causes an infinite loop
+* Return: 0
+*/
+
+int main(void)
+{
+	int num;
+
+	printf("Infinite loop incoming :(\n");
+
+	num = 0;
+
+	/*
+	 * while (num < 10)
+	 * {
+	 * 	putchar(i);
+	 * }
+	 */
+	printf("Infinite loop avoided! \\o/\n");
+	return (0);
+}

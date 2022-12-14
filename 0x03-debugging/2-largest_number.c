b8b441492a1f6c94410abae7a2259a212a14ef0b
David Ukelere <david.ukelere291@gmail.com>
2022-12-14 15:07:13 +0100

    Added largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,7 +12,7 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && a > c)
+	if (a > b && b < c)
 	{
 		largest = a;
 	}
@@ -20,7 +20,7 @@ int largest_number(int a, int b, int c)
 	{
 		largest = c;
 	}
-	else if (b > c)
+	else if (b > c && c < a)
 	{
 		largest = b;
 	}
